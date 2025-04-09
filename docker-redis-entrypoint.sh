292970e4c29087e1fd5657d219b20b9d3bc4e39d
Marcelo Fleury <marcelomf@gmail.com>
2025-04-09 09:26:12 -0300

    fix

--- docker-redis-entrypoint.sh
+++ docker-redis-entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 set -e
 
-redis-server --tls-port 6379 --port 0 \
-    --tls-cert-file /tls/redis.crt \
-    --tls-key-file /tls/redis.key \
-    --tls-ca-cert-file /tls/ca.crt
+redis-server /etc/redis/redis.conf
--- run.sh
+++ run.sh
@@ -4,5 +4,16 @@ cd redis
 export REDIS_VERSION=7.2
 docker pull redis:7.2
 cd ..
+if [ "$(uname)" == "Darwin" ]; then
+	REDIS_PASSWORD=$(uuidgen | shasum | cut -d':' -f3 | cut -d' ' -f1) 
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+	REDIS_PASSWORD=$(uuidgen | sha256sum | cut -d' ' -f1)
+fi
+echo "requirepass $REDIS_PASSWORD" > redis.conf
+echo "tls-port 6379" >> redis.conf
+echo "port 0" >> redis.conf
+echo "tls-cert-file /tls/redis.crt" >> redis.conf
+echo "tls-key-file /tls/redis.key" >> redis.conf
+echo "tls-ca-cert-file /tls/ca.crt" >> redis.conf
 docker compose up -d
-
+echo "REDIS PASSWORD: $REDIS_PASSWORD"
